36e6e8ec8627203dfb93d178d52e3aa0bfb071b3
Sean Campbell <seancampbell04@gmail.com>
2018-11-15 21:53:39 -0500

    Modified source

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello world!')
+        self.response.write('New hello world!')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
