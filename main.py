e7b24d3b06b0f5b7cb58512a3018552cb78f2fcb
Sean Campbell <seancampbell04@gmail.com>
2018-11-15 22:51:10 -0500

    Read from file

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import webapp2
+import features
+import cloudstorage as gcs
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('New hello world!')
+        self.response.write('New hello world again!')
+
+class ApplyFeature(webapp2.RequestHandler):
+    def get(self):
+        text = self.request.get('text')
+        n = int(self.request.get('n'))
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(features.character_ngrams(text, n))
+        self.response.write(read(''))
+'''
+class FilePage(webapp2.RequestHandler):
+    def get(self):
+        doc = self.request.get('doc')
+        file_name = ''
+        gcs_file = gcs.open(file_name)
+        contents = gcs_file.read()
+        gcs_file.close()
+        self.response.write(contents)
+'''
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/apply-feature', ApplyFeature),
 ], debug=True)
