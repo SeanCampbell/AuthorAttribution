9d29656d066e489bbf59841d694ba8355bfbe2a9
Sean Campbell <seancampbell04@gmail.com>
2018-11-17 21:32:19 -0500

    No longer for App Engine

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import features
-import lib.cloudstorage as gcs
+#import lib.cloudstorage as gcs
 import os
 import webapp2
 
-from google.appengine.api import app_identity
+#from google.appengine.api import app_identity
+import tensorflow
+import keras
 
+'''
 def read_gcs_file(file_path):
     bucket_name = os.environ.get(
         'BUCKET_NAME',
@@ -14,7 +17,7 @@ def read_gcs_file(file_path):
     contents = gcs_file.read()
     gcs_file.close()
     return contents
-
+'''
 # As far as I can tell, you can't read from GCS for an app running locally,
 # so this lets us switch easily between the two.
 # https://github.com/GoogleCloudPlatform/appengine-gcs-client/issues/54
@@ -37,8 +40,8 @@ class ApplyFeature(webapp2.RequestHandler):
 class DocPage(webapp2.RequestHandler):
     def get(self):
         doc = self.request.get('doc')
-        contents = read_gcs_file('/author_files/Adams/Defense1.txt')
-        #contents = read_local_file('author_files/Adams/Defense1.txt')
+        #contents = read_gcs_file('/author_files/Adams/Defense1.txt')
+        contents = read_local_file('author_files/Adams/Defense1.txt')
         self.response.write(contents)
 
 
@@ -47,3 +50,10 @@ app = webapp2.WSGIApplication([
     ('/apply-feature', ApplyFeature),
     ('/show-doc', DocPage),
 ], debug=True)
+
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8080')
+
+if __name__ == '__main__':
+    main()
